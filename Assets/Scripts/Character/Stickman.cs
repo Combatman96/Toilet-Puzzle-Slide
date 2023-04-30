5c4ac46e6982521f1b1a1421acddb67bd42ed50c
ChuongPT <phamtienchuong2000@gmail.com>
2023-04-30 15:49:30 +0700

    create new stickman class

---
+++ Assets/Scripts/Character/Stickman.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class Stickman : MonoBehaviour
+{
+    [SerializeField] private Animator m_animator;
+    [SerializeField] Transform m_animationRoot;
+    [SerializeField] Rigidbody2D rb;
+    public static readonly int s_idle = Animator.StringToHash("Idle");
+    public static readonly int s_run = Animator.StringToHash("Run");
+    public static readonly int s_happy = Animator.StringToHash("Happy");
+    public static readonly int s_noPaper = Animator.StringToHash("No_Paper");
+
+    private void PlayAnimation(int state) 
+    {
+        m_animator.CrossFade(state, 0.3f, 0);
+    }
+
+
+    [SerializeField] PathType m_pathType;
+    [SerializeField] PathMode m_pathMode;
+
+    public void MoveAlongPath(Vector2[] path, float duration) 
+    {
+        rb.DOPath(path, duration, m_pathType, m_pathMode, 10, Color.green).OnComplete(()=>
+        {
+            //TODO: Call action when character reach the goal
+        });
+    }
+}
